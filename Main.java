15b27002676b31ba5dbcbed80ac8662c9e11f6c2
krazlwo <12345esd.me@gmail.com>
2025-10-23 10:54:47 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,5 +5,41 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+    //Problem 1
+    int counter = 1;
+    for (int count = 1; count <= 25; count+=2)
+    {
+      System.out.println(count);
+    }
+
+    //Problem 2
+    int c = 1;
+    for (int i = 17; i <= 73; i++)
+    {
+      System.out.println(i + " ");
+
+      if (c == 10)
+    {
+      System.out.println();
+      c = 0;
+    }
+    c++;
+    }
+    //Problem 3
+    System.out.println("Enter number");
+    int theinput = 23;
+    int cr = 1;
+    for (int e = theinput; e <= 50; e++)
+    {
+      System.out.print(e + " ");
+  
+
+      if (cr -- 5)
+      {
+        cr = 0;
+        System.out.println();
+      }
+    }
+
   }
 }
